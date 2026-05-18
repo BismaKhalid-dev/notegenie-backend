deb9ffbff11c897ce6977c3a9d6dfd29c02288f2
sidra khalid <sidrakhalid1314@gmail.com>
2026-05-18 09:09:55 +0500

    add backend route

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
+// import express from 'express';
+// import cors from 'cors';
+// import dotenv from 'dotenv';
+
+// import summarizeRoute from './routes/summarize.route.js';
+// import flashcardRoute from './routes/flashcard.route.js';
+// import quizRoute from './routes/quiz.route.js';
+// import chatRoute from './routes/chat.route.js';
+// import pdfRoute from './routes/pdf.route.js';
+
+// dotenv.config();
+
+// const app = express();
+
+// app.use(
+//   cors({
+//     origin: [process.env.FRONTEND_URL, 'https://note-genie-iota.vercel.app/'],
+    
+//     credentials: true,
+//   })
+// );
+
+// app.use(express.json({ limit: '20mb' }));
+
+// app.get('/', (req, res) => {
+//   res.json({ message: 'NoteGenie Backend Running' });
+// });
+
+// app.use('/api', summarizeRoute);
+// app.use('/api', flashcardRoute);
+// app.use('/api', quizRoute);
+// app.use('/api', chatRoute);
+// app.use('/api', pdfRoute);
+
+// app.use((err, req, res, next) => {
+//   console.log(err);
+
+//   return res.status(500).json({
+//     message: 'Something went wrong',
+//   });
+// });
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -14,8 +61,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, 'https://note-genie-iota.vercel.app/'],
-    
+    origin: [
+      'http://localhost:5173',
+      'https://note-genie-iota.vercel.app'
+    ],
+    methods: ['GET', 'POST'],
     credentials: true,
   })
 );
